3b461f864ac0e6f34b9d8fb89894a1d70cc478f9
Matt Burns <matt@cameraforensics.com>
2025-10-20 23:11:52 +0100

    Update site URL and base path in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://mattburns.github.io/',
+  base: '/cotham-gardens-ptfa',
   integrations: [sitemap(), tailwind()]
 });
