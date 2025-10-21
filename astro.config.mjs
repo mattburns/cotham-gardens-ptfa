4eba61197f2cd81256712c1aa9b50c803b7b47e8
Matt Burns <matt@cameraforensics.com>
2025-10-21 10:05:04 +0100

    Revert "See if decap supports MDX files..."
    This reverts commit d18111512383e4494e23b4def6e9873385411e7c.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cothamgardensptfa.co.uk/',
-  integrations: [sitemap(), tailwind(), mdx()]
+  integrations: [sitemap(), tailwind()]
 });
