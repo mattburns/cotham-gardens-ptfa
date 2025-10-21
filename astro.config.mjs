d18111512383e4494e23b4def6e9873385411e7c
Matt Burns <matt@cameraforensics.com>
2025-10-21 09:54:12 +0100

    See if decap supports MDX files...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cothamgardensptfa.co.uk/',
-  integrations: [sitemap(), tailwind()]
+  integrations: [sitemap(), tailwind(), mdx()]
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -6,7 +6,7 @@ import { glob } from 'astro/loaders';
 
 // 3. Define your collection(s)
 const events = defineCollection({
-  loader: glob({ pattern: '*.md', base: 'src/content/events' }),
+  loader: glob({ pattern: '*.{md,mdx}', base: 'src/content/events' }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
